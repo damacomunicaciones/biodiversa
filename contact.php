07aa55b28f636338a72adf89902b800c4e5f82a6
damacomunicaciones <31963765+damacomunicaciones@users.noreply.github.com>
2017-11-30 11:11:49 -0300

    Update contact.php

--- contact.php
+++ contact.php
@@ -8,7 +8,7 @@
       $email = $_GET['email'];
       $tel = $_GET['tel'];
       $message = $_GET['message'];
-      $client = 'rodrigoburgoscl@gmail.com';
+      $client = 'biodiversa@biodiversa.com';
 
       //Creamos cabecera.
       $headers = 'De' . " " . $email . "\r\n";
